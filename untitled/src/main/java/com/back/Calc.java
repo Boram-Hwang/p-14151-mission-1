493d49aeb1087403657483fd15ea1d386557067b
boramH <ky26212621@gmail.com>
2026-05-05 21:57:27 +0900

    TDD t14

--- untitled/src/main/java/com/back/Calc.java
+++ untitled/src/main/java/com/back/Calc.java
@@ -2,6 +2,22 @@ package com.back;
 
 public class Calc {
     public static int run(String s) {
-        return 2;
+        String[] arr = s.split(" ");
+
+        int result = Integer.parseInt(arr[0]);
+
+        for(int i = 1; i < arr.length; i+=2){
+            String aa = arr[i];
+            int num = Integer.parseInt(arr[i + 1]);
+
+            if(aa.equals("+")) {
+                result += num;
+            } else if(aa.equals("-")) {
+                result -= num;
+            } else if(aa.equals("*")) {
+                result *= num;
+            }
+        }
+        return result;
     }
 }
