fc41440cf014b729d98d0d714361d92be19798e7
boramH <ky26212621@gmail.com>
2026-05-06 00:11:56 +0900

    TDD t17

--- untitled/src/main/java/com/back/Calc.java
+++ untitled/src/main/java/com/back/Calc.java
@@ -1,21 +1,47 @@
 package com.back;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Calc {
     public static int run(String s) {
-        String[] arr = s.split(" ");
+        List<String> list = new ArrayList<>(Arrays.asList(s.split(" ")));
 
-        int result = Integer.parseInt(arr[0]);
+        // 곱셈/나눗셈 계산 먼저
+        for(int i = 1; i < list.size() - 1; i += 2) {
+            String str = list.get(i);
 
-        for(int i = 1; i < arr.length; i+=2){
-            String aa = arr[i];
-            int num = Integer.parseInt(arr[i + 1]);
+            if(str.equals("*") || str.equals("/")) {
+                int num1 = Integer.parseInt(list.get(i - 1));
+                int num2 = Integer.parseInt(list.get(i + 1));
+
+                int result = (str.equals("*") ? num1 * num2 : num1 / num2);
+
+                // 해당 연산 기준 앞 뒤 리스트 제거
+                list.remove(i + 1);
+                list.remove(i);
+                list.remove(i - 1);
+
+                // 제거 후 그 자리에 연산한 result 값 넣기
+                list.add(i - 1, String.valueOf(result));
+
+                // 인덱스는 연산앞자리
+                i -= 2;
+            }
+        }
+
+        // 덧셈/뺄셈 처리
+        int result = Integer.parseInt(list.get(0));
+
+        for(int i = 1; i < list.size(); i+=2){
+            String aa = list.get(i);
+            int num = Integer.parseInt(list.get(i + 1));
 
             if(aa.equals("+")) {
                 result += num;
             } else if(aa.equals("-")) {
                 result -= num;
-            } else if(aa.equals("*")) {
-                result *= num;
             }
         }
         return result;
